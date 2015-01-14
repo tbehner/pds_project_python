1ba09e2f1afaa82fdc7e926d81f64879a0113b2d
tbehner <behner@cs.uni-bonn.de>
2015-01-14 17:19:19 +0100

    Token Ring option added, commands can be stated on prompt

--- distributed_calc.py
+++ distributed_calc.py
@@ -8,29 +8,45 @@ import xmlrpc.client
 from distributed_server import *
 from utility_functions import *
 
-
 parser = OptionParser()
 parser.add_option("-c", "--connect", dest="server_con", help="connect to server with the given ip address and port number", metavar="ADDRESS:PORT")
 parser.add_option("-p", "--port", dest="port", help="open port with given number", metavar="PORT", type="int", default=2222)
+parser.add_option("--token-ring", dest="token_ring", action="store_true", help="set algorithm to token ring", default=False)
 (options,args) = parser.parse_args()
 
 def start_token_ring(server_func):
     while True:
         if len(server_func.known_server_addr)>0:
-            print("HEY! Es ist mindestens ein Server bekannt")
+            print("Got servers, need token...")
             if server_func.got_token:
-                server_func.got_token = False
                 print("Got token from: {}".format(server_func.got_token_from))
-                next_server = get_next_server(server_func)
-                print("Next server {}".format(next_server))
                 time.sleep(5)
-                # FIXME do some important calculations
+                next_server,position = get_next_server(server_func)
+                if next_server is None:
+                    # i.e. wait for new servers
+                    continue
+                print("Next server {}".format(next_server))
                 print("Do important calculations!")
                 # pass token to next server
                 print("Pass on token")
-                con = xmlrpc.client.ServerProxy(get_con_string(next_server))
-                con.set_token(options.port)
+                try:
+                    con = xmlrpc.client.ServerProxy(get_con_string(next_server))
+                    con.set_token(options.port)
+                except:
+                    print("ERROR:\nTrying again in a sec")
+                    time.sleep(1)
+                    continue
+                # give up token when sure someone else got it
+                server_func.got_token = False
         time.sleep(1)
+
+def connect_to_server(connection,server_func):
+    connection = translate_localhost(connection)
+    print("Connecting to other server...")
+    con = xmlrpc.client.ServerProxy(get_con_string(connection))
+    con.registerRemoteServer(options.port)
+    server_func.known_server_addr.append(connection)
+    print("...connected.")
 
 """
 start server thread
@@ -44,28 +60,45 @@ server_thread.start()
 
 if options.server_con is not None:
 #connect to existing network
-    options.server_con = translate_localhost(options.server_con)
-    print("Connecting to other server...")
-    con = xmlrpc.client.ServerProxy(get_con_string(options.server_con))
-    con.registerRemoteServer(options.port)
-    server_func.known_server_addr.append(options.server_con)
-    print("...connected.")
+    print("Server connection string >{}<".format(options.server_con))
+    connect_to_server(options.server_con,server_func)
     print("Initial server list: {}".format(server_func.known_server_addr))
-else:
-    server_func.got_token = True
 
-token_ring_thread = threading.Thread(target=start_token_ring,args=(server_func,))
-token_ring_thread.daemon = True
-token_ring_thread.start()
+if options.token_ring:
+    # if the list of known_servers is empty get yourself a nice token
+    if not options.server_con:
+        server_func.got_token = True
+    token_ring_thread = threading.Thread(target=start_token_ring,args=(server_func,))
+    token_ring_thread.daemon = True
+    token_ring_thread.start()
 
+print("Starting up program")
+print("----------------------------------------")
+print("Commands:")
+print("\"stop\" to shut down the server and stop the program")
+print("\"connect x.x.x.x:y\" to connect to server on ip x.x.x.x and port y")
+print("----------------------------------------")
 
-"""
-wait until shutdown
-"""
+ip4_addr_re = re.compile('(:?\d{1,4}\.){3}\d{1,4}')
+
 try:
+    while True:
+        user_input = input('> ')
+        if re.match('\s*stop',user_input):
+            raise KeyboardInterrupt()
+        if re.search('connect', user_input):
+            mo = re.search('([.\w]+):(\d+)',user_input)
+            print("Matched String >{}< -- >{}< -- >{}<".format(mo.group(0), mo.group(1), mo.group(2)))
+            # match for 'localhost' or an ip4 address
+            if ip4_addr_re.search(mo.group(1)) or re.match('localhost',mo.group(1)):
+                connect_to_server(mo.group(0),server_func)
+
+#wait until shutdown
     server_thread.join()
 except KeyboardInterrupt:
     print("Shutting down...")
+    while server_func.got_token:
+        time.sleep(5)
     print("Unregister in complete list {}".format(server_func.known_server_addr))
     for s in server_func.known_server_addr:
         print("Unregister at server {}".format(s))
--- utility_functions.py
+++ utility_functions.py
@@ -26,18 +26,29 @@ def get_con_string(server):
     return "http://{}/".format(server)
 
 # get next server in list
-def get_next_server(server_func):
+def get_next_server(server_func,position=None):
     next_server = None
+    next_server_idx = None
+
+    # if there is no other known server return None
     if not server_func.known_server_addr:
         return next_server # which is None
+
+    # if there is no previous server, get the first one
     if server_func.got_token_from is None:
         # start at first element
-        next_server = server_func.known_server_addr[0]
+        return (server_func.known_server_addr[0],0)
+
+    # if a position was given on the command line
+    if position is not None:
+        next_server_idx = position + 1
     else:
-        # get next one
         next_server_idx = server_func.known_server_addr.index(server_func.got_token_from)+1
-        if next_server_idx >= len(server_func.known_server_addr):
-            # start over
-            next_server_idx = 0
+
+    # get next one
+    if next_server_idx >= len(server_func.known_server_addr):
+        # start over
+        next_server_idx = 0
         next_server = server_func.known_server_addr[next_server_idx]
-    return next_server
+
+    return (next_server, next_server_idx)
