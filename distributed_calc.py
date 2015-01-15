a95a81455c167cec173683a6db035aac0bcb53ab
tbehner <behner@cs.uni-bonn.de>
2015-01-15 06:29:34 +0100

    Zwischencommit Christophers Änderungen

--- distributed_calc.py
+++ distributed_calc.py
@@ -7,35 +7,76 @@ from xmlrpc.server  import SimpleXMLRPCRequestHandler
 import xmlrpc.client
 from distributed_server import *
 from utility_functions import *
-
-"""
-TODOs:
-    * Hier alles bereinigen, was sich über die Zeit angesammelt hat
-    * mit Alexander kommunizieren wie wir die Algorithmen aufteilen
-    * Algorithmus implementieren
-"""
+from netifaces import AF_INET, AF_INET6, AF_LINK, AF_PACKET, AF_BRIDGE
+import netifaces as ni
 
 parser = OptionParser()
 parser.add_option("-c", "--connect", dest="server_con", help="connect to server with the given ip address and port number", metavar="ADDRESS:PORT")
-parser.add_option("-p", "--port", dest="port", help="open port with given number", metavar="PORT", type="int", default=2222)
+parser.add_option("-p", "--port", dest="port", help="open port with given number", metavar="PORT", type="int", default=0)
 (options,args) = parser.parse_args()
+
+def print_own_ip_addresses(port):
+    ip_list = ni.interfaces()
+    print("----------------------------------------")
+    print("Server is listening to:")
+    for ip in ip_list:
+        print(ni.ifaddresses(ip)[AF_INET][0]['addr'] + ":" + str(port))
+    print("----------------------------------------")
+
+def start_token_ring(server_func):
+    while True:
+        if len(server_func.known_server_addr)>0:
+            print("HEY! Es ist mindestens ein Server bekannt")
+            if server_func.got_token:
+                server_func.got_token = False
+                print("Got token from: {}".format(server_func.got_token_from))
+                next_server = get_next_server(server_func)
+                print("Next server {}".format(next_server))
+                time.sleep(2)
+                # FIXME do some important calculations
+                print("Do important calculations!")
+                # pass token to next server
+                print("Pass on token")
+                print("Server " + next_server)
+                print("Server " + get_con_string(next_server))
+                con = xmlrpc.client.ServerProxy(get_con_string(next_server))
+                con.ServerFunctions.acceptToken(str(options.port))
+        time.sleep(1)
 
 """
 start server thread
 """
 server = SimpleXMLRPCServer(("",options.port),ChattyRequestHandler)
-server_func = ServerFunctions()
-server.register_instance(server_func)
+# reset port, in case the port was arbitrary set by system
+options.port = server.socket.getsockname()[1]
+server_func = ServerFunctions(options.port)
+server.register_function(server_func.registerRemoteServer, 'ServerFunctions.registerRemoteServer')
+server.register_function(server_func.unregisterRemoteServer, 'ServerFunctions.unregisterRemoteServer')
+server.register_function(server_func.refreshRemoteServerList, 'ServerFunctions.refreshRemoteServerList')
+server.register_function(server_func.acceptToken, 'ServerFunctions.acceptToken')
+#server.register_instance(server_func, True)
 server_thread = threading.Thread(target=start_serving,args=(server,))
 server_thread.daemon = True
 server_thread.start()
 
+print_own_ip_addresses(options.port)
+
 if options.server_con is not None:
 #connect to existing network
+    options.server_con = translate_localhost(options.server_con)
     print("Connecting to other server...")
     con = xmlrpc.client.ServerProxy(get_con_string(options.server_con))
-    con.register_new_server(options.port)
+    con.ServerFunctions.registerRemoteServer(str(options.port))
+    server_func.known_server_addr.append(options.server_con)
     print("...connected.")
+    print("Initial server list: {}".format(server_func.known_server_addr))
+else:
+    server_func.got_token = True
+
+token_ring_thread = threading.Thread(target=start_token_ring,args=(server_func,))
+token_ring_thread.daemon = True
+token_ring_thread.start()
+
 
 """
 wait until shutdown
@@ -43,13 +84,9 @@ wait until shutdown
 try:
     server_thread.join()
 except KeyboardInterrupt:
-    if not options.server_con is None:
-        con = xmlrpc.client.ServerProxy(get_con_string(options.server_con))
-        con.remove_server(options.port)
-    else:
-        for s in server_func.known_server_addr:
-            con = xmlrpc.client.ServerProxy(get_con_string(s))
-            con.remove_server(options.port)
-            break
     print("Shutting down...")
-
+    print("Unregister in complete list {}".format(server_func.known_server_addr))
+    for s in server_func.known_server_addr:
+        print("Unregister at server {}".format(s))
+        con = xmlrpc.client.ServerProxy(get_con_string(s))
+        con.ServerFunctions.unregisterRemoteServer(options.port)
--- distributed_server.py
+++ distributed_server.py
@@ -11,47 +11,65 @@ from utility_functions import *
 class ChattyRequestHandler(SimpleXMLRPCRequestHandler):
     log = []
     def __init__(self, request, client_address, server):
-        # Ich weiß noch nicht an welcher Stelle ich das hier überhaupt abrufen kann
         ChattyRequestHandler.log.append(client_address)
         SimpleXMLRPCRequestHandler.__init__(self, request, client_address, server)
 
 class ServerFunctions:
-    def __init__(self):
+    def __init__(self, own_port):
         self.known_server_addr = []
+        self.got_token = False
+        self.got_token_from = None
+        self.next_token_server = None
+        self.lock = threading.Lock()
+        self.own_port = own_port
 
-    def __populate_servers():
-        # FIXME hier ist irgendwo ein Bug
+    def __populate_servers(self):
         for server in self.known_server_addr:
+            print("Populate server list to {}".format(server))
             # all addresses are populated, except the address of the server the list gets populated to
             populated_servers = self.known_server_addr.copy()
             populated_servers.remove(server)
+            populated_servers.insert(0, str(self.own_port))
             con = xmlrpc.client.ServerProxy(get_con_string(server))
-            con.check_server_list(populated_servers)
+            con.ServerFunctions.refreshRemoteServerList(populated_servers)
 
 # 
 # methods to be served
 #
-    def remove_server(self,client_port):
+    def unregisterRemoteServer(self,client_port):
         server = get_addr_string(ChattyRequestHandler.log[-1][0],client_port)
-        print("Saying bye to server {}".format(server))
-        self.known_server_addr.remove(server)
-        #populate_servers()
-        return 0
+        if server in self.known_server_addr:
+            print("Saying bye to server {}".format(server))
+            self.known_server_addr.remove(server)
+        else:
+            print("Bye from unknown server{}".format(server))
+        print("Updated server list {}".format(self.known_server_addr))
+        return 1
 
-    def register_new_server(self,client_port):
-        # FIXME das muss vielleicht angepasst werden (formattierung nicht klar)
+    def registerRemoteServer(self,client_port):
         server_addr = get_addr_string(ChattyRequestHandler.log[-1][0],client_port)
-        print("Incomming connection: {}".format(server_addr))
-        print("Known servers: {}".format(self.known_server_addr))
+        print("Register new server {}".format(server_addr))
         if server_addr not in self.known_server_addr:
-            print("New server {}".format(server_addr))
             self.known_server_addr.append(server_addr)
-            #populate_servers()
+        self.__populate_servers()
+        print("New Server list: {}".format(self.known_server_addr))
         return 1
 
-    def check_server_list(self,server_list):
-        self.known_server_addr = server_list
-        #print(servers)
+    def refreshRemoteServerList(self,server_list):
+        print("Get new server list {}".format(server_list))
+        for i in range(count(server_list)):
+            if i==0:
+                server = get_addr_string(ChattyRequestHandler.log[-1][0], server_list[i])
+            else:
+                server = server_list[i]
+            if server not in self.known_server_addr:
+                self.known_server_addr.append(server)
+        print("New Server list: {}".format(self.known_server_addr))
         return 1
 
+    def acceptToken(self,client_port):
+        print("I got the token!")
+        self.got_token = True
+        self.got_token_from = get_addr_string(ChattyRequestHandler.log[-1][0],client_port)
+        return 1
 
